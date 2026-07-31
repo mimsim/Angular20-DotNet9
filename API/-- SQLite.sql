a203fa4ec439188bea2a881c34290e0772d49f33
mimsim <mimsim2000@gmail.com>
2026-07-31 07:18:38 +0300

    add API endpoints from get members and member id

---
+++ API/-- SQLite.sql
@@ -0,0 +1,6 @@
+-- SQLite
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('bob-id', 'Bob', 'bob@example.com');
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('tom-id', 'tom', 'tom@example.com');
--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using API;
+using API.Entities;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-
+public DbSet<AppUser> Users { get; set; }
     // public DbSet<WeatherForecast> WeatherForecasts { get; set; }
 }
